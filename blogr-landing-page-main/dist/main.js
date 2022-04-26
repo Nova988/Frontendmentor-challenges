512dde44a2f6226880da31ef9db239ca980cf12c
Nova988 <antonionovak1998@hotmail.com>
2022-04-26 20:20:16 +0200

    added desktop navbar

--- blogr-landing-page-main/dist/main.js
+++ blogr-landing-page-main/dist/main.js
@@ -7,26 +7,51 @@ const categoryBtn = Array.from(document.querySelectorAll(".category__title"));
 const categoryElements = Array.from(
   document.querySelectorAll(".category__subjects")
 );
+const categoryIcon = document.querySelectorAll(".category__icon");
+const menuOverlay = document.getElementById("menu-overlay");
+const mediaLarge = window.matchMedia("(min-width: 900px)");
 
-hamburgerMenu.addEventListener("click", function () {
-  iconMenu.classList.toggle("open");
+if (!mediaLarge.matches) {
+  hamburgerMenu.addEventListener("click", function () {
+    // Show mobile menu
+    menuOverlay.classList.toggle("hide");
 
-  if (iconMenu.classList.contains("open")) {
-    iconMenu.src = "./images/icon-close.svg";
-  } else {
-    iconMenu.src = "./images/icon-hamburger.svg";
-  }
-});
+    // Change icon of het hamburger menu
+    iconMenu.classList.toggle("open");
 
-console.log(categoryElements);
+    if (iconMenu.classList.contains("open")) {
+      iconMenu.src = "./images/icon-close.svg";
+    } else {
+      iconMenu.src = "./images/icon-hamburger.svg";
+    }
+  });
+}
 
+// Change category Icon image color
+if (mediaLarge.matches) {
+  categoryIcon.forEach((x) => (x.src = "./images/icon-arrow-light.svg"));
+}
+
+// Navigation functionality
 categoryBtn.forEach((x, i) => {
   x.addEventListener("click", function () {
+    // delete opacity of category title
+    categoryBtn.forEach((el) => el.classList.remove("category--open"));
+
+    // Open menu options on click of category title
     categoryElements.forEach((box, j) => {
       if (i === j) {
         box.classList.toggle("hide");
+
+        if (!box.classList.contains("hide")) {
+          categoryIcon[j].style.transform = "rotate(180deg)";
+          x.classList.add("category--open");
+        } else {
+          categoryIcon[j].style.transform = "rotate(0deg)";
+        }
       } else {
         box.classList.add("hide");
+        categoryIcon[j].style.transform = "rotate(0deg)";
       }
     });
   });
